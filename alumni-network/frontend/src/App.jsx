60423b9d746dedc1f893ead62eb80abe265f7660
Rewant-1 <rewant.bhriguvanshi@gmail.com>
2025-12-06 22:35:14 +0530

    deleted

--- alumni-network/frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import AlumniMap from './map/components/AlumniMap';
-
-export default function App(){
-  return <div style={{padding:20}}><AlumniMap useMock={true} /></div>;
-}
-// export default App;
